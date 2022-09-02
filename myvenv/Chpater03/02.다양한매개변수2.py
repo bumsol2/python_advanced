c0b0a8c661ba7149c31a18a524c22df06c8ecb70
bumsol2 <bumsol2@gmail.com>
2022-09-03 06:47:58 +0900

    0903 매개변수

--- myvenv/Chpater03/02.다양한매개변수2.py
+++ myvenv/Chpater03/02.다양한매개변수2.py
@@ -11,5 +11,15 @@ print_fruits('apple','orange','mango','grape')
 def comment_info(**kwargs):
     for key,value in kwargs.items():
         print(f'{key}:{value}')
-        
+
 comment_info(name='파린이', content='정말 감사합니다!')
+
+# 매개변수 작성 순서
+# 위치 - 기본 - 위치 가변 - 키워드(기본) - 키워드 가변 외울 필요는 없다.
+
+def post_info(*tags, title, content, **kwargs): 
+    print(f'제목 : {title}')
+    print(f'내용 : {content}')
+    print(f'태그 : {tags}')
+
+post_info('#파이썬', '#함수',title='파이썬 함수 정리!', content='다양한 매개변수 정리합니다.')
